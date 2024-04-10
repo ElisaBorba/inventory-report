db7a183038c2b9e14829767eeb849e806268789e
ElisaBorba <elisaborbaa@gmail.com>
2024-04-10 18:05:51 -0300

    req 4

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -1,6 +1,7 @@
 from typing import Dict, List, Type
 from abc import ABC, abstractmethod
 from inventory_report.product import Product
+import json
 
 
 class Importer(ABC):
@@ -11,8 +12,23 @@ class Importer(ABC):
     def import_data(self) -> List[Product]: ...
 
 
-class JsonImporter:
-    pass
+class JsonImporter(Importer):
+    def import_data(self) -> List[Product]:
+        product_list = []
+
+        try:
+            with open(self.path, "r") as file:
+                json_data = file.read()
+                products = json.loads(json_data)
+
+                for product_dict in products:
+                    product_list.append(Product(**product_dict))
+
+        except (FileNotFoundError, json.JSONDecodeError) as e:
+
+            print(f"Erro ao importar dados: {e}")
+
+        return product_list
 
 
 class CsvImporter:
