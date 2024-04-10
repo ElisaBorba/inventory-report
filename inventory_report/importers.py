a35c136baaaa5f9b20e3b0e7790a62528247d6f6
ElisaBorba <elisaborbaa@gmail.com>
2024-04-10 17:11:40 -0300

    req 4

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -1,8 +1,14 @@
-from typing import Dict, Type
+from typing import Dict, List, Type
+from abc import ABC, abstractmethod
+from inventory_report.product import Product
 
 
-class Importer:
-    pass
+class Importer(ABC):
+    def __init__(self, path: str) -> None:
+        self.path = path
+
+    @abstractmethod
+    def import_data(self) -> List[Product]: ...
 
 
 class JsonImporter:
